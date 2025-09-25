a6255d5d28203230f3f8024d8d247438ecd702dd
Mateus dos Santos Pereira <mateus@n4050-debian>
2025-09-24 22:21:29 -0300

    Expediente

--- src/database/database.sql
+++ src/database/database.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS registers (
     birth DATE NOT NULL,
     email VARCHAR(100) UNIQUE PRIMARY KEY,
     password VARCHAR(200) NOT NULL,
-    /* 0: manager, 1: patient, 2: doctor */
+    /* 0: administrador, 1: paciente, 2: médico */
     type TINYINT NOT NULL CHECK (type IN (0, 1, 2))
 );
 
@@ -32,49 +32,64 @@ CREATE TABLE IF NOT EXISTS hospitals (
     ) STORED
 );
 
-CREATE TABLE IF NOT EXISTS doctors (
-    email VARCHAR(100) PRIMARY KEY,
-    field VARCHAR(25) NOT NULL,
-    id_hospital INT NOT NULL,
+CREATE TABLE IF NOT EXISTS expedients (
+    id_expedient INT NOT NULL
+        PRIMARY KEY AUTO_INCREMENT,
     duration TIME NOT NULL,
-    FOREIGN KEY (email) 
-        REFERENCES registers(email)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-    FOREIGN KEY (id_hospital) 
-        REFERENCES registers(id_hospital)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    description VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS schedules (
-    id_schedule INT NOT NULL
-    PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS weekdays (
+    id_expedient INT NOT NULL,
     weekday VARCHAR(13) NOT NULL
         CHECK (weekday IN (
-            "Domingo",
-            "Segunda-feira",
-            "Terça-feira",
-            "Quarta-feira",
-            "Quinta-feira",
-            "Sexta-feira",
-            "Sábado"
+            0, 1, 2, 3, 5, 6
+            /*
+            0: Domingo,
+            1: Segunda-feira,
+            2: Terça-feira,
+            3: Quarta-feira,
+            4: Quinta-feira,
+            5: Sexta-feira,
+            6: Sábado
+            */
         )),
+    week INT NOT NULL DEFAULT(1),
     start TIME NOT NULL,
     end TIME NOT NULL,
     break TIME,
     time_break TIME,
-    UNIQUE (email, weekday),
-    FOREIGN KEY (email)
-        REFERENCES doctors(email)
+    UNIQUE(weekday, week),
+    FOREIGN KEY (id_expedient)
+        REFERENCES expedients(id_expedient)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    PRIMARY KEY(expedients, weekday, week)
+);
+
+CREATE TABLE IF NOT EXISTS doctors (
+    email VARCHAR(100) PRIMARY KEY,
+    field VARCHAR(25) NOT NULL,
+    id_hospital INT NOT NULL,
+    id_expedient INT NOT NULL,
+    first_day DATE NOT NULL CURRENT_DATE,
+    FOREIGN KEY (email) 
+        REFERENCES registers(email)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (id_hospital) 
+        REFERENCES hospitals(id_hospital)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (expedients) 
+        REFERENCES expedients(id_expedient)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS appointments (
     id_appointment INT NOT NULL
-    PRIMARY KEY AUTO_INCREMENT,
+        PRIMARY KEY AUTO_INCREMENT,
     email_doctor VARCHAR(100) NOT NULL
         REFERENCES doctors(email)
         ON DELETE CASCADE
